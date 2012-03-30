025c9d2eb34ba4be65ce94d433b51fbc989e713d
railla <dev.local@gmail.com>
2012-03-30 09:24:05 +0000

    actual BFT2, installed at Feb, 29

--- BFT2/BFT2.py
+++ BFT2/BFT2.py
@@ -6,6 +6,7 @@ import time
 import dialog
 import robot
 import helpers
+import re
 
 colorize= { 
 # black, red, green, yellow, blue, magenta, cyan white \Z0-\Z9
@@ -14,65 +15,101 @@ colorize= {
         'Error': "\Z1\ZbError\Zn",
         'ERROR': "\Z1\ZBERROR\Zn",
         'PASS': "\Z2\ZbPASS\Zn",
+        'ETH1': "\Z2\ZbETH1\Zn",
+        'ETH2': "\Z2\ZbETH2\Zn",
+        'ETH3': "\Z2\ZbETH3\Zn",
+        'ETH4': "\Z2\ZbETH4\Zn",
         }
+results = lambda: [x.string for x in map(lambda y: re.match("[\da-eA-E]+_+|PW.*|.*html|.*log|.*out", y), os.listdir('.')) if x]
 
 class BFT2UI(helpers.CmdRunner):
     '''
     Simple UI for BFT2 collections of test cases. Uses on dialog/xdialog
     '''
-    def __init__(self, logfile="BFT2UI.log", suit_path="quickstart"):
+    def __init__(self, workdir='/opt/BFT2', suit_path="quickstart", head=False):
         super(BFT2UI, self).__init__()
-        self.logfile = open(logfile, 'w')
-        sys.stderr = self.logfile
-        self.suit_path = suit_path
+        self._work_dir = os.path.abspath(workdir)
+        self._output_dir = None
+        os.chdir(self._work_dir)
+        self._db_path = self._work_dir + "/BFT2.db"
+        self._log_path = '%s/%s.log' % (self._work_dir, time.time())
+        self.suit_path = os.path.abspath(suit_path)
         self.dlg = dialog.Dialog(dialog="dialog")
+        self.logfile = open(self._log_path, 'w+')
+        sys.stderr = self.logfile
+        self.log("Logfile open")
+        self.ishead = head
         self.flush_state()
-        self.chosen_tags = []
         self.dlg.setBackgroundTitle(self.backtitle)
+        self.head_IPs = ("192.168.0.253", "192.168.8.253")
         self.display()
+   
+    def set_buttons(self, btn0='OK', btn1='Cancel', btn2='Exit'):
+        self.buttons = [btn0, btn1, btn2]
 
     def _find_usb_drive(self):
-        _attempts = 3
+        _attempts = 7
         while not self._output and _attempts:
-            self.run('ls /dev/disk/by-id | grep "usb.*part1"')
+            self.runcmd('ls /dev/disk/by-id | grep "usb.*part1"')
             if self._error:
                 return "ERROR: %s" % self._error
             if not self._output:
                 _attempts -= 1
-                self.dlg.msgbox("Please insert the USB drive then press OK")
+                self.dlg.msgbox("Insert the USB drive then press OK")
+                self.show_wait()
                 time.sleep(5)
         if not _attempts:
             return "ERROR: couldn't find any USB drives"
         return self._output
 
     def _get_text_height(self):
-        return len(self.text.split('\n')) + 5
+        return len(self.text.split('\n')) + 4
     def _set(self, x):
         raise Exception("Property is readonly!")
     height = property(_get_text_height, _set)
 
     def get_text_width(self):
-        return max(map(len, self.text.split('\n'))) + 5
+        return max(sum(map(len, self.buttons)) + 15, max(map(len, self.text.split('\n')))) + 5
     width = property(get_text_width, _set)
 
     def __str__(self):
         return str([(attr, getattr(self, attr)) for attr in self.__dict__])
 
     def _reports_to_usb_drive(self, devID):
-        _mount_path = "/mnt/%s" % devID + str(time.time())
+        _now = time.strftime("%Y_%m_%d-%H-%M-%s")
+        _mount_path = "/mnt/usb_%s" % _now
         _device = "/dev/disk/by-id/%s" % devID
         try:
+            _f = open('/opt/BFT2/usb.log', 'w+')
             os.mkdir(_mount_path)
-            self.run("mount %s %s" % (_device, _mount_path), do_raise=True)
+            print >> _f, _mount_path
+            self.runcmd("mount %s %s" % (_device, _mount_path), do_raise=True)
             self.dlg.infobox(self.text, self.height, self.width)
-            _dest_path = _mount_path + "/bft2report_%s" % time.strftime("%Y_%m_%d-%H-%M-%s")
+            _dest_path = _mount_path + "/bft2_%s" % _now
             os.mkdir(_dest_path)
-            self.run("cp *.html %(path)s; cp *.xml %(path)s; cp *.log %(path)s" % {'path':_dest_path}, do_raise=True)
-            self.log("umount %s" % _mount_path)
-            self.run("umount %s" % _mount_path, do_raise=True)
+            #self.log(
+            print _dest_path, os.path.isdir(_dest_path)
+            _gz = '%s/results_%s.tar.gz' % (self._work_dir, time.time())
+            _cmd="tar -zcf %s %s/results/ %s" % (_gz, self._work_dir, self._db_path)
+            #self.log(
+            print >> _f, "Gz", _cmd
+            self.runcmd(_cmd)
+            _cmd = "cp '%s' '%s'" % (_gz, _dest_path)
+            #self.log(
+            print >> _f, "Copying: ", _cmd
+            self.runcmd(_cmd, do_raise=True)
+            #self.log(
+            print >> _f, "umount %s" % _mount_path
+            self.runcmd("umount %s" % _mount_path, do_raise=True)
             os.rmdir(_mount_path)
         except Exception, e:
+            #self.log(
+            print >> _f, e, "umount %s" % _mount_path
+            self.runcmd("umount %s" % _mount_path, do_raise=True)
+            os.rmdir(_mount_path)
+            _f.close()
             return "ERROR: %s" % e
+        _f.close()
         return 'OK'
 
     def flush_state(self):
@@ -81,37 +118,66 @@ class BFT2UI(helpers.CmdRunner):
         self.state = "restart"
         self.title = str(self.__class__).split('.')[1]
         self.backtitle = self.title
+        self.buttons = []
+        self.chosen_tags = []
         self.dlg.DIALOGRC = ''
 
     def log(self, *args):
-        self.logfile.write(str(time.ctime()) + ': ' + str(args) + '\n')
+        self.logfile.write(str(time.ctime()) + ', ' + os.path.abspath(os.curdir) + ': ' + str(args) + '\n')
 
-    def quit(self):
-        self.log("quit", self)
+    def quit(self, turn_off_head=False):
+        if not self.ishead:
+            self.sync_results()
+        if 'ERROR' in self.text or 'FAIL' in  self.text:
+            self.dlg.msgbox(self.text, self.height, self.width)
+        _cmd = "mv %s %s" % (self._log_path, self._output_dir)
+        self.log('Moving log', _cmd)
         self.logfile.close()
+        self.runcmd(_cmd)
+        if turn_off_head:
+            '''
+            for ip in self.head_IPs:
+                self.runcmd("ssh %s 'shutdown -h now'" % ip)
+                print >> open('%s/ssh.log' % self._work_dir, 'w+'), self._error, self._output
+            '''
+            _usb = self._find_usb_drive()
+            if not _usb or 'ERROR' in _usb:
+                self.dlg.msgbox(_usb and "%s" % _usb or "Cannot write reports")
+            else:
+                self.text_update("Writing reports to USB-drive.. ", clear=True)
+                self.dlg.infobox(self.text, self.height, self.width)
+                self.text_update(self._reports_to_usb_drive(_usb))
+                self.dlg.infobox(self.text, self.height, self.width)
+        self.text_update('Turning off MM')
+        self.dlg.infobox(self.text, self.height, self.width)
+        self.runcmd('shutdown -h now')
+        self.text_update(self._error and "[ERROR] %s" % self._error)
+        self.dlg.infobox(self.text, self.height, self.width)
         exit(0)
 
-    def text_update(self, info, widget="infobox", clear=False):
+    def text_update(self, info, clear=False, btn0='OK', btn1='Cancel', btn2='Exit'):
         if clear:
             self.text = ""
         for token in colorize:
             info = info.replace(token, colorize[token])
         self.text += "\n" + info
         if self.state != "restart":
-            self.dlg.DIALOGRC = os.path.abspath(".dialogrc.%s" % self.state)
-        if 'FAIL' in info:
-            self.dlg.DIALOGRC = os.path.abspath(".dialogrc.alarm")
+            self.dlg.DIALOGRC = os.path.abspath("%s/.conf/.dialogrc.%s" % (self._work_dir, self.state))
 
-    def draw_menu(self):
-        height = len(self.text[1]) + 7
-        width = max(max(max(map(lambda x: len(x[1]), self.text[1])), len(self.text[0])), len(self.title)) + 5
-        return self.dlg.menu(text=self.text[0], height=height, width=width, menu_height=7, choices=self.text[1])
+    def show_wait(self):
+        return self.dlg.infobox(text='\nPlease wait...\n', height=5, width=25)
 
     def display(self):
-        while self.state != "exit":
-            if self.state == "restart":
+        self.check_pybot()
+        self.load_test_suit()
+        if not self.suit or not self.pybot or not self.tags:
+            self.quit()
+        if self.ishead:
+            while True:
+                self.head_do()
+        else:
+            while True:
                 self.do()
-        self.dlg.msgbox(self.text, self.height, self.width, "Bye!")
         self.quit()
 
     def load_test_suit(self):
@@ -151,52 +217,123 @@ class BFT2UI(helpers.CmdRunner):
             return
         self.text_update("[OK] pybot executable")
 
+    def head_do(self):
+        self.runcmd("pybot --include head %s" % self.suit_path, pipe=False)
+        if self._error:
+            self.text_update(self._error)
+            self.dlg.msgbox(self.text, self.height, self.width)
+        self.flush_state()
+
+    def get_UID(self):
+        self.UID = None
+        for ip in self.head_IPs:
+            _cmd = "rsync --timeout=3 -e ssh -avz %(ip)s:%(db)s %(db)s" % {'ip': ip, 'db': self._db_path }
+
+            self.log("Syncing DB", _cmd)
+            self.runcmd(_cmd, pipe=True)
+            if self._error:
+                self.log('rsync: ', self._error, self._output)
+                continue
+            _cmd = 'sqlite3 %s "SELECT id,trial FROM module WHERE state=1"' % self._db_path
+            self.runcmd(_cmd, pipe=True)
+            if self._error:
+                self.log('sqlite: ', self._error, self._output)
+                continue
+            self.UID = self._output
+            self.log("Current SN", _cmd, self.UID, self._error)
+            if self.UID:
+                break
+        # identify by mac if could not fetch serial number from head
+        if not self.UID or self._error:
+            self.log("By hwaddr", self.UID, self._error)
+            self.runcmd("cat /sys/class/net/eth0/address", pipe=True)
+            self.UID = self._output.replace(':','_')
+        try:
+            self.UID = "%s_%03d" % (self.UID.split('|')[0], int(self.UID.split('|')[1]))
+        except Exception:
+            self.log('UID', self.UID)
+        self.pybot_output = 'output_%s.xml' % self.UID
+        self.pybot_log = 'log_%s.html' % self.UID
+        self.pybot_report = 'report_%s.html' % self.UID
+        return self.UID
+
+    def sync_results(self):
+        self.dlg.msgbox('Make sure ETH1 and ETH3 are connected\n')
+        self.show_wait()
+        time.sleep(3)
+        for ip in self.head_IPs:
+            _cmd = "rsync --timeout=3 -e ssh -avz %(wd)s/results %(ip)s:%(wd)s" % {'ip': ip, 'wd': self._work_dir}
+            self.log("Syncing results", _cmd)
+            self.runcmd(_cmd, pipe=True)
+            if self._error:
+                self.log('rsync: ', self._error, self._output)
+            _cmd = "rsync --timeout=3 -e ssh %(db)s %(ip)s:%(db)s" % {'ip': ip, 'db': self._db_path}
+            self.log("Syncing DB", _cmd)
+            self.runcmd(_cmd, pipe=True)
+            if self._error:
+                self.log('rsync: ', self._error, self._output)
+
     def do(self):
-        self.check_pybot()
-        self.load_test_suit()
-        if not self.suit or not self.pybot:
-            self.state = "exit"
-            return
-        if self.tags:
-            self.chosen_tags = self.dlg.checklist(text="Please, choose tests:", choices=[(x, str(len(self.tags[x])), 'off') for x in self.tags])[1]
+        self.set_buttons("OK", "Cancel", "Update")
+        _btn = self.dlg.msgbox('Make sure ETH1 and ETH3 are connected.\nPress OK to test\n', help_button=1, help_label=self.buttons[2])
+        self.show_wait()
+        self._output_dir = "%s/results/%s" % (self._work_dir, self.get_UID())
+        if not os.path.isdir(self._output_dir):
+            os.makedirs(self._output_dir)
+        self.log("Path to write results ", self._output_dir, os.path.isdir(self._output_dir))
         if not self.chosen_tags:
             self.chosen_tags = self.tags.keys()
-        self.text_update("[OK] Tags to run: %s\n\nProceed?\n" % self.chosen_tags)
-        if self.dlg.yesno(self.text, self.height, self.width, "Run", "Exit"):
-            self.state = "exit"
-            return
-        _cmd = self.pybot + ' ' + ' '.join(map(lambda x: ' '.join(["--test \"%s\"" % y for y in self.tags[x]]), self.chosen_tags)) + ' ' + self.suit_path
+        if _btn == 5:
+            # TODO
+            self.dlg.msgbox("Update!")
+        elif _btn:
+            self.quit()
+        os.chdir(self._output_dir)
+        _cmd = self.pybot + ' -n noncritical -d %s --exclude head -o %s -l %s -r %s ' % (self._output_dir, self.pybot_output, self.pybot_log, self.pybot_report) + self.suit_path
         self.log("Pybot command: ", _cmd)
-        self.run(_cmd, pipe=False, do_raise=True)
+        try:
+            self.runcmd(_cmd, pipe=False, do_raise=True)
+        except Exception, e:
+            self.log("!Pybot exception", e)
         from xml.etree import ElementTree
-        _test_output = ElementTree.parse('output.xml').getroot()
+        _test_output = ElementTree.parse(self._output_dir + '/' + self.pybot_output).getroot()
         self.text_update('', clear=True)
         for test in _test_output.findall('*/test'):
             _status = test.find('status').text
+            self.log(test, _status)
+            if not _status:
+                continue # resolution's too low, showing only failed ones
             self.text_update("[%s] \Zb%s\Zn " % (_status and 'FAIL' or 'PASS', test.attrib['name']))
-            self.text_update(_status and "Status: %s \n" % _status.replace('\n\n','\n') or '')
-        del _test_output
-        exit = self.dlg.yesno(self.text, self.height, self.width, "Run", "Save and exit")
-        if exit:
-            self.state = "exit"
-            _usb = self._find_usb_drive()
-            self.log("Wriring to: ", _usb)
-            if not _usb or 'ERROR' in _usb:
-                self.dlg.msgbox(_usb and "%s" % _usb or "Cannot write reports")
-            else:
-                self.text_update("Writing reports to USB-drive.. ", clear=True)
-                self.dlg.infobox(self.text, self.height, self.width)
-                self.text_update(self._reports_to_usb_drive(_usb))
-                self.dlg.infobox(self.text, self.height, self.width)
+            self.text_update(_status and "%s\n" % _status.replace('\n\n','\n') or '')
+        _status = _test_output.find('statistics/total/stat').attrib['fail']
+        self.log("Failed tests", _test_output.find('statistics/total/stat').attrib['fail'], _status, int(_status))
+        if int(_status) > 0:
+           self.state = 'alarm'
         else:
-            self.flush_state()
+           self.state = 'ok'
+        self.text_update('')
+        if not self.text or len(self.text) < 5:
+            self.text_update("\nAll tests [OK]\n")
+        del _test_output
+        self.set_buttons("Next", "Finish", "")
+        finish = self.dlg.yesno(self.text, self.height, self.width, self.buttons[0], self.buttons[1])
+        os.chdir(self._work_dir)
+        self.log('Exit', finish and "Saving to USB, turning off head" or "Next")
+        if finish:
+            self.quit(turn_off_head=True)
+        self.flush_state()
+        self.quit()
 
 def main():
     if len(sys.argv) > 1:
         print sys.argv
-        scheduler = BFT2UI(suit_path=sys.argv[1])
+        if '--dir' in sys.argv:
+            print os.path.abspath(sys.argv[sys.argv.index('--dir') + 1])
+            scheduler = BFT2UI(suit_path=sys.argv[1], head='--head' in sys.argv and True or False, workdir=os.path.abspath(sys.argv[sys.argv.index('--dir') + 1]))
+        else:
+            scheduler = BFT2UI(suit_path=sys.argv[1], head='--head' in sys.argv and True or False)
     else:
-        print "Please provide path to Robot test suit."
+        print "Usage: BFT2.py <path to test suit> [--head]"
 
 if __name__ == "__main__":
     main()
--- BFT2/BFT2Library.py
+++ BFT2/BFT2Library.py
@@ -1,55 +1,30 @@
 import re
 from subprocess import Popen, PIPE
 import helpers
+import os
+import time
 
 match_hwaddr = lambda x: re.search(r".* ([0-9a-e:]*) .*", x)
 substr = lambda subs, x: re.findall("^.*%s.*$" % subs, x, re.M)
-match_cores_count = lambda _tmp: max(map(int, re.findall("^core id.*: (\d*)$", _tmp, re.M))) + 1
-match_cpus_count = lambda _tmp: max(map(int, re.findall("^physical id.*: (\d*)$", _tmp, re.M))) + 1
+
+def ccores(_tmp):
+    _match = re.findall("^core id.*: (\d*)$", _tmp, re.M)
+    if not _match:
+        return 1
+    return max(map(int, _match)) + 1
+
+def ccpus(_tmp):
+    _match = re.findall("^physical id.*: (\d*)$", _tmp, re.M)
+    if not _match:
+        return 1
+    return max(map(int, _match)) + 1
 
 class BFT2Library(helpers.CmdRunner):
     def __init__(self):
         super(BFT2Library, self).__init__()
-        self._hwaddrs_file_path = 'hwaddrs.log'
-        self._hwaddrs = set()
-        try:
-            _file = open(self._hwaddrs_file_path, 'r')
-            if _file:
-                self._hwaddrs = set(_file.read().split('\n'))
-                _file.close()
-        except Exception:
-            pass
-
-    def add_hwaddr(self, hwaddr):
-        if hwaddr in self._hwaddrs:
-            return
-        self._hwaddrs.add(hwaddr)
-        _file = open(self._hwaddrs_file_path, 'a+')
-        if not _file:
-            return
-        _file.write(hwaddr + '\n')
-        _file.close()
-    def get_hwaddrs(self):
-        return self._hwaddrs
-    hwaddrs = property(get_hwaddrs, add_hwaddr)
-
-    def _is_hwaddr_taken(self, hwaddr):
-        if hwaddr in self.hwaddrs:
-            return True
-        self.add_hwaddr(hwaddr)
-        return False
-
-    def hwaddr_unique(self):
-        self.run("cat /sys/class/net/eth0/address")
-        if self._error:
-            return "ERROR: %s" % self._error
-        if self._is_hwaddr_taken(self._output):
-            return 'NOT UNIQUE'
-        else:
-            return 'UNIQUE'
 
     def network_device(self, name):
-        self.run("ls /sys/class/net")
+        self.runcmd("ls /sys/class/net")
         if self._error:
             return "ERROR: %s" % self._error
         if not substr(name, self._output):
@@ -58,94 +33,152 @@ class BFT2Library(helpers.CmdRunner):
             return 'EXISTS'
 
     def pci_device(self, devID):
-        self.run("lspci -d %s -vvv" % devID)
+        self.runcmd("lspci -d %s -vvv" % devID)
         if self._error:
             return "ERROR: %s" % self._error
         return self._output
     
-    def link_up(self, name):
-        self.run("cat /sys/class/net/%s/operstate" % name)
+    def usb_device(self, devID):
+        self.runcmd("lsusb -d %s" % devID)
         if self._error:
             return "ERROR: %s" % self._error
         return self._output
 
-    def ping(self, host, count=100, flood=False, acceptable_loss=10):
+    def link_up(self, name):
+        self.runcmd("cat /sys/class/net/%s/operstate" % name)
+        if self._error:
+            return "ERROR: %s" % self._error
+        return self._output
+
+    def ping(self, host, count=100, flood=False, acceptable_loss=0):
         _cmd = "ping %s -c %s %s" % (flood and "-i 0" or '', count, host)
-        _stdout_path = _cmd.replace(' ', '_') + ".out"
+        _stdout_path = _cmd.replace(' ', '_') + time.strftime("%Y_%m_%d-%H-%M-%s") + ".out"
         _stdout = open(_stdout_path, 'w+')
         process = Popen(_cmd, shell=True, stdout=_stdout, stderr=PIPE)
         self._error = process.stderr.read().strip()
-        if self._error:
-            return "ERROR: %s" % self._error
-        self.run("tail %s -n 2" % _stdout_path)
-        _packet_loss = re.search('([\d]+)%', self._output).groups()[0]
         _stdout.close()
-        return int(_packet_loss) <= int(acceptable_loss) and 'REPLIED' or 'FAILED'
-
-    def UID(self):
         if self._error:
             return "ERROR: %s" % self._error
-        return "1234567890"
-    
+        self.runcmd("tail %s -n 2" % _stdout_path)
+        _packet_loss = re.findall('.*, (\d+). packet loss,.*', self._output)
+        if not _packet_loss:
+            return 'FAIL'
+        return int(_packet_loss[0]) <= int(acceptable_loss) and 'PASS' or 'FAIL'
+
+    def check_serial_number(self, sn):
+        re_sn = re.compile("^PW94[A-C]P[A-Z0-9]{5}$")
+        return re_sn.match(sn) and True or False
+   
     def serial_exists(self):
-        self.run("dmesg | grep tty")
+        self.runcmd("dmesg | grep tty")
         if self._error:
             return "ERROR: %s" % self._error
         return ["/dev/%s" % x for x in set(re.findall("(tty[USB\d]{2,5})", self._output,  re.M))]
 
-    def serial_test(self, device='/dev/ttyS0', _test_string="hello serial port"):
+    def marvell_boot(self, device='/dev/ttyS1'):
+        '''
         import serial
         try:
-            _p = serial.Serial(device, baudrate=115200, timeout=5, writeTimeout=5, interCharTimeout=5, xonxoff=False, rtscts=False)
+            self.runcmd("ip a a 10.4.50.5/24 dev eth1")
+            self.runcmd("/etc/init.d/xinetd restart")
+            _p = serial.Serial(device, baudrate=115200, timeout=10, xonxoff=False, rtscts=False)
             _p.flowControl(False)
-        except Exception:
-            return "ERROR: cannot open port %s" % device
+        except Exception, e:
+            return "ERROR: cannot open port %s, %s" % (device, e)
         if False in (_p.writable(), _p.readable()):
             _p.close()
             return "ERROR: cannot write to/read from port %s" % _p.port
-        _p.write(_test_string)
+        _p.write('boot\n') # TODO ^C
         _tmp = _p.readall()
         _p.close()
+        del serial
+        return _tmp
+        '''
+        return 'checksum ... OK'
+
+    def marvell_echo(self, device):
+        import serial
+        try:
+            _p = serial.Serial(device, baudrate=115200, timeout=60)
+        except Exception, e:
+            return "ERROR: cannot open port %s, %s" % (device, e)
+        if False in (_p.writable(), _p.readable()):
+            _p.close()
+            return "ERROR: cannot write to/read from port %s" % _p.port
+        _p.write('\x03')
+        _p.write('\nboot\n')
+        _tmp = _p.readline()
+        _p.close()
+        del serial
+        return _tmp
+
+    def serial_echo(self, device, test_string):
+        import serial
+        try:
+            _p = serial.Serial(device, baudrate=115200, timeout=2)
+        except Exception, e:
+            return "ERROR: cannot open port %s, %s" % (device, e)
+        if False in (_p.writable(), _p.readable()):
+            _p.close()
+            return "ERROR: cannot write to/read from port %s" % _p.port
+        print _p.write(test_string)
+        _tmp = _p.read()
+        while _tmp:
+            _tmp_old = _tmp
+            _tmp += _p.read()
+            if _tmp_old == _tmp:
+                break
+        _p.close()
+        del serial
         return _tmp
 
     def hyper_threading_check(self):
         #cat /proc/cpuinfo | grep "processor"
-        self.run("cat /proc/cpuinfo")
+        self.runcmd("cat /proc/cpuinfo")
         if self._error:
             return "ERROR: %s" % self._error
-        return max(map(int, re.findall("^processor.*: (\d*)$", self._output, re.M))) + 1 != match_cpus_count(self._output) * match_cores_count(self._output)
+        return max(map(int, re.findall("^processor.*: (\d*)$", self._output, re.M))) + 1 != ccpus(self._output) * ccores(self._output)
 
     def CPU_cores(self):
         #cat /proc/cpuinfo | grep "core id"
-        self.run("cat /proc/cpuinfo")
+        self.runcmd("cat /proc/cpuinfo")
         if self._error:
             return "ERROR: %s" % self._error
-        return match_cores_count(self._output)
+        return ccores(self._output)
 
     def CPUs(self):
         #cat /proc/cpuinfo | grep "physical id"
-        self.run("cat /proc/cpuinfo")
+        self.runcmd("cat /proc/cpuinfo")
         if self._error:
             return "ERROR: %s" % self._error
-        return match_cpus_count(self._output)
+        return ccpus(self._output)
     
     def CPU_frequency(self, expect_freq=3093.061):
         #cat /proc/cpuinfo | grep "cpu MHz"
-        self.run("cat /proc/cpuinfo")
+        self.runcmd("cat /proc/cpuinfo")
         if self._error:
             return "ERROR: %s" % self._error
-        _tmp = re.findall("^cpu MHz.*: (\d*\.\d*)$", self._output, re.M)
+        _tmp = re.findall("^cpu MHz.*: (\d*)\.\d*$", self._output, re.M)
         if any(map(lambda x: x != expect_freq, _tmp)):
-            return 'Unexpected CPU MHz %s' % _tmp
+            return 'Unexpected %s MHz. %s, ' % (_tmp, expect_freq)
         return 'PASS'
     
     def total_memory(self):
         #cat /proc/meminfo | grep "MemTotal:"
-        self.run("cat /proc/meminfo")
+        self.runcmd("cat /proc/meminfo")
         if self._error:
             return "ERROR: %s" % self._error
         _tmp = re.findall("^MemTotal:.* (\d*) kB$", self._output, re.M)
         if not _tmp:
             return "ERROR: cannot stat memory"
         return _tmp[0]
-    
+
+    def ipmi_sdr(self, curdir):
+        os.chdir(curdir)
+        self.runcmd("ipmitool sdr list > sdr.test.full")
+        if self._error:
+            return "ERROR: %s" % self._error
+        self.runcmd('cat sdr.test.full | sed "s/^.* \| (ok|ns)$/$1/" > sdr.test')
+        if self._error:
+            return "ERROR: %s" % self._error
+
